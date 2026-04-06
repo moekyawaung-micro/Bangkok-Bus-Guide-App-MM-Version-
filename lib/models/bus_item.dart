cc6191a0e37ef301e00562e6fd0268eb65e8b55e
moekyawaung-micro <moekyawaung.mka@outlook.com>
2026-04-06 16:37:21 +0630

    Create bus_item.dart

---
+++ lib/models/bus_item.dart
@@ -0,0 +1,17 @@
+class BusItem {
+  final String id;
+  final String busNo;
+  final String route;
+  final String thai;
+  final String my;
+  final String en;
+
+  const BusItem({
+    required this.id,
+    required this.busNo,
+    required this.route,
+    required this.thai,
+    required this.my,
+    required this.en,
+  });
+}
